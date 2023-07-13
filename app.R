65f1fe97cbfee17dfb1a2ff8955e51110dda8c40
Andres-AM <andes.acunamarroquin@gmail.com>
2023-07-13 16:39:33 +0200

    shiny app with rnorm data set and ggplot

--- app.R
+++ app.R
@@ -5,30 +5,54 @@ library(dplyr)
 
 # UI
 ui <- fluidPage(
-  titlePanel("Scatter Plot"),
+  titlePanel("Chick Weight Visualization"),
   sidebarLayout(
     sidebarPanel(
       sliderInput("bins", "Width in Jitter:", min = 0.1, max = 1, value = 0.2, step = 0.1),
-      selectInput("x_variable", "Select X Variable:", choices = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")),
-      selectInput("y_variable", "Select Y Variable:", choices = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"))
+      selectInput("x_variable", "Select X Variable:", choices = c("Time", "Chick")),
+      selectInput("y_variable", "Select Y Variable:", choices = c("weight", "Diet"))
     ),
     mainPanel(
-      plotOutput("plot")
+      plotOutput("scatter_plot"),
+      plotOutput("boxplot"),
+      plotOutput("barplot")
     )
   )
 )
 
 # Server
 server <- function(input, output) {
-  # Load the iris dataset
-  dataset <- iris
+  # Load the ChickWeight dataset
+  dataset <- ChickWeight
   
   # Generate scatter plot
-  output$plot <- renderPlot({
-    p <- ggplot(dataset, aes(x = .data[[input$x_variable]], y = .data[[input$y_variable]], color = Species)) +
+  output$scatter_plot <- renderPlot({
+    p <- ggplot(dataset, aes(x = .data[[input$x_variable]], y = .data[[input$y_variable]], color = Diet)) +
       geom_jitter(width = input$bins, height = 0, size = 2, alpha = 0.5) +
-      labs(title = "Scatter Plot", x = input$x_variable, y = input$y_variable) +
+      labs(title = "Chick Weight Scatter Plot", x = input$x_variable, y = input$y_variable) +
       scale_color_brewer(palette = "Set1") +
+      theme_minimal()
+    
+    p
+  })
+  
+  # Generate boxplot
+  output$boxplot <- renderPlot({
+    p <- ggplot(dataset, aes(x = Diet, y = weight, fill = Diet)) +
+      geom_boxplot(alpha = 0.5) +
+      labs(title = "Chick Weight Boxplot", x = "Diet", y = "Weight") +
+      scale_fill_brewer(palette = "Set1") +
+      theme_minimal()
+    
+    p
+  })
+  
+  # Generate barplot
+  output$barplot <- renderPlot({
+    p <- ggplot(dataset, aes(x = Chick, fill = Diet)) +
+      geom_bar() +
+      labs(title = "Chick Count by Diet", x = "Chick", y = "Count") +
+      scale_fill_brewer(palette = "Set1") +
       theme_minimal()
     
     p
---
+++ app_bis.R
@@ -0,0 +1,74 @@
+# Required libraries
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+# UI
+ui <- fluidPage(
+  titlePanel("Density Plot Visualization"),
+  sidebarLayout(
+    sidebarPanel(
+      div(
+        h4("Group 1 Parameters"),
+        numericInput("n1", "Sample Size:", value = 200),
+        numericInput("mean1", "Mean Value:", value = 10),
+        numericInput("sd1", "Standard Deviation:", value = 1)
+      ),
+      div(
+        h4("Group 2 Parameters"),
+        numericInput("n2", "Sample Size:", value = 200),
+        numericInput("mean2", "Mean Value:", value = 12),
+        numericInput("sd2", "Standard Deviation:", value = 1)
+      ),
+      actionButton("refresh", "Refresh")
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+# Server
+server <- function(input, output, session) {
+  # Generate dataset 1 using rnorm
+  dataset1 <- reactive({
+    data <- rnorm(input$n1, mean = input$mean1, sd = input$sd1)
+    data
+  })
+  
+  # Generate dataset 2 using rnorm
+  dataset2 <- reactive({
+    data <- rnorm(input$n2, mean = input$mean2, sd = input$sd2)
+    data
+  })
+  
+  # Generate density plot with colored groups
+  output$plot <- renderPlot({
+    data <- data.frame(
+      Group = rep(c("Group 1", "Group 2"), each = c(input$n1, input$n2)),
+      Value = c(dataset1(), dataset2())
+    )
+    
+    p <- ggplot(data, aes(x = Value, fill = Group)) +
+      geom_density(alpha = 0.5) +
+      labs(title = "Density Plot Visualization", x = "Value", y = "Density") +
+      scale_fill_manual(values = c("Group 1" = "blue", "Group 2" = "red")) +
+      theme_minimal()
+    
+    p
+  })
+  
+  # Refresh button event
+  observeEvent(input$refresh, {
+    updateNumericInput(session, "n1", value = 200)
+    updateNumericInput(session, "mean1", value = 10)
+    updateNumericInput(session, "sd1", value = 1)
+    updateNumericInput(session, "n2", value = 200)
+    updateNumericInput(session, "mean2", value = 20)
+    updateNumericInput(session, "sd2", value = 1)
+    updateSliderInput(session, "bins", value = 0.2)
+  })
+}
+
+# Run the app
+shinyApp(ui, server)
